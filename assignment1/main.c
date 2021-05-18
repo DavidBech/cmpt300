06e3f8e76ea1884814034a21dc113c43e814b30e
David Bechert <dbechert@sfu.ca>
2021-05-18 16:15:45 -0700

    Added Makefile and neccessary files

---
+++ assignment1/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+#include "list.h"
+
+/* Test File For list.c */
+
+int main(){
+    int status = LIST_SUCCESS;
+    printf("Hello, World\n");
+    printf("status: %d\n", status);
+    return status;
+}
