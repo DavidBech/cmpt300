0e7b2a4d239c02669ddcf8fa6509709dec356009
David Bechert <dbechert@sfu.ca>
2021-06-08 16:14:22 -0700

    cast inputs into correct variable types

--- assignment2/src/main.c
+++ assignment2/src/main.c
@@ -3,11 +3,30 @@
 
 #include "list.h"
 
+#define INVALID_INPUT 1
+
 int main(int argc, char** argv){
-    printf("[0]: %s \n", argv[0]);
-    printf("[1]: %s \n", argv[1]);
-    printf("[2]: %s \n", argv[2]);
-    printf("[3]: %s \n", argv[3]);
+    char *endptr;
+    long in_port;
+    long out_port;
+    char *out_machine;
+
+    in_port = strtol(argv[1], &endptr, 10);
+    if(endptr == argv[1]){
+        fprintf(stderr, "Invalid In Port input: %s", argv[1]);
+        exit(INVALID_INPUT);
+    }
+    out_machine = argv[2];
+    out_port = strtol(argv[3], &endptr, 10);
+    if(endptr == argv[3]){
+        fprintf(stderr, "Invalid In Port input: %s", argv[3]);
+        exit(INVALID_INPUT);
+    }
+
+    printf("in_port: %ld \n", in_port);
+    printf("out_machine: %s \n", out_machine);
+    printf("out_port: %ld \n", out_port);
+
     return(0);
 }
 
