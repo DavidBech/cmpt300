7b17b34a2a305b49999ede021a5537d28031cda8
David Bechert <dbechert@sfu.ca>
2021-07-02 22:36:16 -0700

    finished TODO list

--- assignment2/headers/stalk.h
+++ assignment2/headers/stalk.h
@@ -1,7 +1,7 @@
 #ifndef _STALK_H_A2
 #define _STALK_H_A2
 
-// Larger messages handled thourgh
+// Larger messages handled thourgh multiple packets
 #define MAX_MESSAGE_SIZE 1024
 #define STALK_MAX_NUM_NODES LIST_MAX_NUM_NODES
 #define TERMINATION_STRING "!\n\0"
